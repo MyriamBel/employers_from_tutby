6585c0761d8c71746eec2ca9f6dc52330abaee01
MyriamBel <31003974+MyriamBel@users.noreply.github.com>
2018-01-16 16:04:36 +0300

    Restructurize code
    The structure of the site has changed - the error of finding the next link is fixed.
    Сhanged the data type to the dictionary.
    Сompany name search is added.

--- vacancyparser.py
+++ vacancyparser.py
@@ -1,49 +1,60 @@
 #/usr/bin/python3
 # -*- coding: utf-8 -*-
+# This module
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
 base_url = 'https://jobs.tut.by'
-start_url = base_url + '/employers_company/informacionnye_tekhnologii_sistemnaya_integraciya_internet' #start page
-
+start_url = base_url + '/employers_company/informacionnye_tekhnologii_sistemnaya_integraciya_internet'  # start page
 
 list_re = [
-    '^(/employers_company/informacionnye_tekhnologii_sistemnaya_integraciya_internet/page-)\d+$', #for find next page
-    '^(/employer/)\d+$' #for current page
+    '^(/employers_company/informacionnye_tekhnologii_sistemnaya_integraciya_internet/page-)\d+$',  # for find next page
+    '^(/employer/)\d+$'  # for current page
 ]
 
 name_class = [
-    'b-pager__next', #for next page
-    'employers-company__list employers-company__list_companies' #for current page
+    'employers-company__pager',  # for next page
+    'employers-company__list employers-company__list_companies'  # for current page
 ]
 
-def getelement(url, name_class, string_re):
-    page = urlopen(url, timeout=3)     #open page
-    bsObj = BeautifulSoup(page.read(), "html.parser") #create BeautifulSoup Object
-    container = bsObj.find('div', {'class': name_class})    #find div from BS Object
-    obj_from_container = container.findAll('a', href=re.compile(string_re)) #find a href to next page or employer
-    listofurls = []
-    for i in obj_from_container:
-        listofurls.append(i.get('href')) #get all urls
-    return listofurls
 
-def writeinfile(urllist):
+def getelement(url, name_class,
+               string_re):  # Получаем на вход строку ссылки, строку имени класса тега, строку регулярки
+    page = urlopen(url, timeout=3)  # open page
+    bsObj = BeautifulSoup(page.read(), "html.parser")  # create BeautifulSoup Object
+    container = bsObj.find('div', {'class': name_class})  # find div from BS Object
+    obj_from_container = container.findAll('a', href=re.compile(string_re))  # find a href to next page or employer
+    dictionary_employers = {}
+    if name_class != 'employers-company__pager':    #если это поиск ссылок на компании
+        for i in obj_from_container:
+            dictionary_employers[i.get('href')] = i.get_text()
+    else:
+        if obj_from_container[-1].get_text() == 'дальше':   #Если это поиск ссылки на след.страницу
+            dictionary_employers['next_link'] = obj_from_container[-1].get('href')
+    return dictionary_employers  # Возвращаем словарь найденных ссылок(из 1 элемента, если это ссылка на некст пэйдж)
+
+
+def writeinfile(anydict):  # Пишем в файл словарь, который передадим в функцию.
     with open('list employers.txt', 'a') as f:
-        for i in urllist:
+        for i in anydict:
+            f.write(anydict[i] + '\n')
             f.write('URL:' + '\n')
             f.write(i + '\n')
             f.write('----------' + '\n')
 
-def getemployers(url, name_class, list_re):
-    global base_url
-    list_of_employers = getelement(url, name_class[1], list_re[1])
-    writeinfile(list_of_employers)
-    next_link = getelement(url, name_class[0], list_re[0])
+
+def getemployers(base_url, url, name_class, list_re):  # Получаем строку ссылки, строку имени класса тега, строку регулярки
+    dict_of_employers = getelement(url, name_class[1], list_re[1])  # Получаем ссылки на текущей странице
+    writeinfile(dict_of_employers)  # Пишем все найденные ссылки в файл
+    print(url)
+    next_link = list(getelement(url, name_class[0], list_re[0]).values())  # Получаем следующую ссылку
     print(next_link)
     if len(next_link) > 0:
-        getemployers(url=base_url + next_link[-1], name_class=name_class, list_re=list_re)
+        getemployers(base_url=base_url, url=base_url + next_link[0], name_class=name_class,
+                     list_re=list_re)  # Переходим по след.ссылке и начинаем работу заново
     else:
-        pass
+        pass  # Необязательно писать это, может в будущем чего-нибудь допишу. Пока мне нравится так, пусть будет.
 
-getemployers(url=start_url, name_class=name_class, list_re=list_re)
+
+getemployers(base_url=base_url, url=start_url, name_class=name_class, list_re=list_re)
